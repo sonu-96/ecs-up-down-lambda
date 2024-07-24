2876d7890766e981b9d149a009c2e862103e0c7d
sonu yadav <155170525+sonu-96@users.noreply.github.com>
2024-07-24 17:55:14 +0530

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,6 +4,16 @@ import { EC2Client, StartInstancesCommand, StopInstancesCommand } from '@aws-sdk
 const ecsClient = new ECSClient({ region: process.env.AWS_REGION || 'ap-south-1' });
 const ec2Client = new EC2Client({ region: process.env.AWS_REGION || 'ap-south-1' });
 
+/*
+ecs =>  
+{
+  "cluster": "divamtech-staging",
+  "services": ["divamtech-staging-server-service", "divamtech-staging-worker-service"],
+  "desiredCount": 1
+}
+ec2 => { "ec2InstanceIds": ["i-0362c8fc8eb38bc36"], "ec2Action": "start" }
+*/
+
 export const handler = async (event) => {
   try {
     console.log('event', event);
@@ -30,13 +40,12 @@ export const handler = async (event) => {
       const ec2InstanceIds = event.ec2InstanceIds;
       const ec2Action = event.ec2Action || 'stop'; // default to stop if not provided
 
+      const ec2Params = { InstanceIds: ec2InstanceIds };
       if (ec2Action === 'start') {
-        const startParams = { InstanceIds: ec2InstanceIds };
-        await ec2Client.send(new StartInstancesCommand(startParams));
+        await ec2Client.send(new StartInstancesCommand(ec2Params));
         console.log(`EC2 instances ${ec2InstanceIds} have been started.`);
       } else if (ec2Action === 'stop') {
-        const stopParams = { InstanceIds: ec2InstanceIds };
-        await ec2Client.send(new StopInstancesCommand(stopParams));
+        await ec2Client.send(new StopInstancesCommand(ec2Params));
         console.log(`EC2 instances ${ec2InstanceIds} have been stopped.`);
       } else {
         throw Error(`Invalid EC2 action: ${ec2Action}. Allowed actions are 'start' and 'stop'.`);
